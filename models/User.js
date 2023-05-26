0b45931dc4bac0030fe9144813c8ea46a2e6c9ac
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-05-25 20:27:50 -0700

    added User model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return `${this.username} has ${this.friends.length} friends.`;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
