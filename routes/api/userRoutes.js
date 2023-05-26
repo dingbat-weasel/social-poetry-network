a2791b2334184b72938267ae76b743246d06cf44
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-05-26 10:17:33 -0700

    controllers added for users, get, getsingle, create, update

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -18,4 +18,5 @@ router.route("/:userId/friends").post(createFriend);
 router.route("/:userId/friends/:friendId").delete(deleteFriend);
 
 // These routes use functions that must be written and brought in from ./controllers
-// After this poemRoutes must be made with similar required functions (and reaction routes)
+
+module.exports = router;
