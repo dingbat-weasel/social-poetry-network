71eaf402e815178b3c709ad117a839b074f7dce1
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-05-25 21:21:45 -0700

    userRoutes updated, still /controller functions to be written and poemRoutes/reactionRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,4 +3,20 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser,
+  deleteUser,
+  createFriend,
+  deleteFriend,
 } = require("../../controllers/userController");
+
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router
+  .route("/:userId/friends/:friendId")
+  .post(createFriend)
+  .delete(deleteFriend);
+
+// These routes use functions that must be written and brought in from ./controllers
+// After this poemRoutes must be made with similar required functions (and reaction routes)
